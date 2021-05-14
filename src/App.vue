e0147c62268f50dd53d7d1ce792abf7a82319371
Nerlih Max <nerlihmax@yandex.ru>
2021-05-15 01:02:09 +0700

    Подчистил автосгенерированный мусор

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </div>
   <div class="container">
     <router-view />
