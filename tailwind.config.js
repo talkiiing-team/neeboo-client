20b2a1a732a6dd6e9eaf2e7f2f605ba90a83b846
roamiiing <unf.hwm@ya.ru>
2021-05-15 01:29:45 +0700

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,17 @@ module.exports = {
     container: {
       center: true,
     },
-    extend: {
-    },
     fontFamily: {
       montserrat: 'Montserrat, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       roboto: 'Roboto, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
+    fontSize: {
+      4: '1.3125rem',
+      3: '1.125rem',
+      2: '1rem',
+      1: '0.8125rem',
+    },
+    extend: {
     },
   },
   variants: {
