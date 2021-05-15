0dafd0c7a93c72862427950f4fd0597ce701fc6b
Nerlih Max <nerlihmax@yandex.ru>
2021-05-15 20:08:51 +0700

    Создал Wallet

---
+++ src/views/Wallet.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col justify-start items-start">
+    <h1 class="font-montserrat font-bold font-size text-3xl leading-7">{{ networth }} $</h1>
+    <h2 class="m4">Счета:</h2>
+    <div class="">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'Wallet',
+  components: {
+
+  },
+  setup() {
+    const networth = ref(1337228);
+
+    const assets = ref([
+      {
+
+      },
+      {
+
+      },
+    ]);
+
+    return {
+      networth,
+      assets,
+    };
+  },
+};
+</script>
