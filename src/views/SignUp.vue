b1770ea790d6e0ef5ccb87e3c3f55e3535e678ce
Nerlih Max <nerlihmax@yandex.ru>
2021-05-15 20:01:16 +0700

    прикол

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,39 +1,55 @@
 <template>
-  <form class="flex flex-col justify-between items-center h-full">
+  <form class="flex flex-col justify-between items-center h-full"
+    @submit.prevent="register">
     <div class="flex flex-col justify-start items-start w-full">
-      <a class="m4 mb-4">Создание аккаунта</a>
-      <VSelect class="mb-8" :options="options" v-model="selected"></VSelect>
-      <template v-if="selected == 'individual'">
+      <p class="m4 mb-4">Создание аккаунта</p>
+      <template v-if="!phase">
+        <VSelect class="mb-8" :options="options" v-model="selected"></VSelect>
+        <template v-if="selected == 'individual'">
+          <VInput class="w-full mb-4"
+            placeholder="Фамилия" v-model="surname"></VInput>
+          <VInput class="w-full mb-4"
+            placeholder="Имя" v-model="name"></VInput>
+          <VInput class="w-full mb-6"
+            placeholder="Отчество" v-model="patronymic"></VInput>
+        </template>
+        <template v-else>
+          <VInput class="w-full mb-6"
+            placeholder="Название компании" v-model="companyName"></VInput>
+        </template>
         <VInput class="w-full mb-4"
-          placeholder="Фамилия" v-model="surname"></VInput>
+          placeholder="Дата регистрации" v-model="registrationDate"></VInput>
         <VInput class="w-full mb-4"
-          placeholder="Имя" v-model="name"></VInput>
+          placeholder="ОГРНИП" v-model="orgnip"></VInput>
         <VInput class="w-full mb-6"
-          placeholder="Отчество" v-model="patronymic"></VInput>
+          placeholder="Регистрирующий орган" v-model="registrator"></VInput>
+        <VInput class="w-full mb-8"
+          placeholder="Свидетельство о регистрации" v-model="regPaperId"></VInput>
       </template>
       <template v-else>
         <VInput class="w-full mb-6"
-          placeholder="Название компании" v-model="companyName"></VInput>
+          placeholder="Логин" v-model="login"></VInput>
+        <VInput class="w-full mb-4"
+          placeholder="Пароль" type="password" v-model="password"></VInput>
+        <VInput class="w-full mb-4"
+          placeholder="Повторите пароль" type="password" v-model="secondPassword"></VInput>
+        <div>
+          <p>Вы пидор</p>
+        </div>
       </template>
-      <VInput class="w-full mb-4"
-        placeholder="Дата регистрации" v-model="registrationDate"></VInput>
-      <VInput class="w-full mb-4"
-        placeholder="ОГРНИП" v-model="orgnip"></VInput>
-      <VInput class="w-full mb-6"
-        placeholder="Регистрирующий орган" v-model="registrator"></VInput>
-      <VInput class="w-full mb-8"
-        placeholder="Свидетельство о регистрации" v-model="regPaperId"></VInput>
     </div>
     <VButton class="self-center justify-self-end mb-16">Продолжить</VButton>
   </form>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, toRefs, ref } from 'vue';
 
 import VSelect from '@/components/VSelect.vue';
 import VInput from '@/components/VInput.vue';
 import VButton from '@/components/VButton.vue';
+
+import { useActions, actionsMap } from '@/store/index';
 
 export default {
   name: 'SignUp',
@@ -43,6 +59,8 @@ export default {
     VButton,
   },
   setup() {
+    const actions = useActions([actionsMap.register]);
+
     const options = [
       {
         display: 'Индивидуальный предприниматель',
@@ -54,23 +72,49 @@ export default {
       },
     ];
 
-    const form = {
-      surname: ref(''),
-      name: ref(''),
-      patronymic: ref(''),
-      companyName: ref(''),
-      registrationDate: ref(''),
-      orgnip: ref(''),
-      registrator: ref(''),
-      regPaperId: ref(''),
+    const phase = ref(false);
+
+    const form = reactive({
+      surname: '',
+      name: '',
+      patronymic: '',
+      companyName: '',
+      registrationDate: '',
+      orgnip: '',
+      registrator: '',
+      regPaperId: '',
+      login: '',
+      password: '',
+    });
+
+    const secondPassword = ref('');
+
+    const register = async () => {
+      if (phase.value === true) {
+        console.log(form);
+        if (form.password.value !== secondPassword.value) {
+          // toggle сообщение что мы обьебались с паролями
+          return;
+        }
+        try {
+          await actions.register(form);
+        } catch (ex) {
+          console.error(ex);
+        }
+      } else {
+        phase.value = true;
+      }
     };
 
     const selected = ref(options[0].value);
 
     return {
-      ...form,
+      ...toRefs(form),
       options,
       selected,
+      register,
+      phase,
+      secondPassword,
     };
   },
 };
